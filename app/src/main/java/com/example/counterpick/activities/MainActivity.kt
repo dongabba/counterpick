ff1270c751eddb13070bd9b9b6c2dbfb9fad66e5
Жалейко Александр Сергеевич <AZhaleyko@vimpelcom.ru>
2019-09-23 17:48:17 +0300

    initial commit

---
+++ app/src/main/java/com/example/counterpick/activities/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.counterpick.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.counterpick.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
